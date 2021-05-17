e2118eb0f6f6ef42e219dbce011dfb97aa134714
BadariNarayana <badarinarayana557@gmail.com>
2021-05-17 17:34:43 +0530

    Initial Commit

--- bankingapp/Bankapp/Bankapp/settings.py
+++ bankingapp/Bankapp/Bankapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=vglch^+cyi^e(fr)uy@403_+#)v!+ek8em_py69c39@avm155'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sparkbankings.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
